5caffb66f5b9ac627aed9bb497c6d0bee37f8896
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-11-02 16:53:27 -0400

    built tables for game, ran migrations

---
+++ app/Day.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Day extends Model
+{
+    //
+	public function condition() {
+		return $this->belongsTo('App\Condition');
+	}
+}
